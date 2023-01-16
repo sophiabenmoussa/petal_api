89ee1a1c2892263cbb03480a303390ff444e602b
Sophia Benmoussa <sophiabenmoussa6@gmail.com>
2023-01-16 17:18:40 +0100

    some details

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,9 @@
 class PokemonsController < ApplicationController
-  BOOKS_PER_PAGE = 12
+  POKEMONS_PER_PAGE = 12
 
   def index
     @page = params.fetch(:page,0).to_i
-    @pokemons = Pokemon.offset(@page * BOOKS_PER_PAGE).limit(BOOKS_PER_PAGE)
+    @pokemons = Pokemon.offset(@page * POKEMONS_PER_PAGE).limit(POKEMONS_PER_PAGE)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   acts_as_token_authenticatable
 
+  has_many :pokemons
 end
